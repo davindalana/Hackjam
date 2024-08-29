9b140105445a66b2f510201298d19eec9179d8b0
davindalana <46117700+davindalana@users.noreply.github.com>
2024-08-29 21:54:28 +0700

    mainmenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+    public void LoadGame()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
